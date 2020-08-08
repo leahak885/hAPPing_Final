b08847e9ffa432ecc7ac55b5031ba661e74b767f
Leah Kvares <leahaviv@icloud.com>
2020-08-08 15:48:13 -0400

    commit 2

---
+++ hAPPing_Final/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  hAPPing_Final
+//
+//  Created by Leah on 8/8/20.
+//  Copyright © 2020 Leah Kvares. All rights reserved.
+//
+
+import UIKit
+class ToDo {
+    var name = ""
+    var important = false
+}
